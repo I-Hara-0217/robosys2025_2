adc672a6005ba6612a1fcfecf25474593280105b
I.Hara <i.hara.git@gmail.com>
2025-12-30 06:48:41 +0900

    test.bashの修正

--- test.bash
+++ test.bash
@@ -8,12 +8,15 @@ ng () {
 
 res=0
 
+export PYTHONUNBUFFERED=1
+export RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED=1
+
 source /opt/ros/jazzy/setup.bash
 cd ~/ros2_ws
 colcon build --packages-select saying_sender
 source install/setup.bash
 
-timeout 20s ros2 run saying_sender quotes_publisher > /tmp/saying_sender.log || true
+timeout 25s ros2 run saying_sender quotes_publisher > /tmp/saying_sender.log 2>&1 || true
 
 count=$(grep -c "Publish:" /tmp/saying_sender.log)
 
